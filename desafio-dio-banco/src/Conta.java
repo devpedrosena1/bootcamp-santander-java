dbe8476be6894a55e2e3ce70ea153443d2293dbf
devpedrosena1 <devpedrosena1@gmail.com>
2025-07-09 14:25:49 -0300

    terminando desafio de conta de banco bootcamp-santander

--- desafio-dio-banco/src/Conta.java
+++ desafio-dio-banco/src/Conta.java
@@ -1,8 +1,16 @@
 public abstract class Conta implements IConta {
 
-    private int agencia;
-    private int numero;
-    private double saldo;
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+
+    private static int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
+
+    public Conta() {
+        this.agencia = Conta.AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+    }
 
     public int getAgencia() {
         return agencia;
@@ -19,16 +27,23 @@ public abstract class Conta implements IConta {
 
     @Override
     public void sacar(double valor) {
-
+        this.saldo -= valor;
     }
 
     @Override
     public void depositar(double valor) {
-
+        this.saldo += valor;
     }
 
     @Override
     public void transferir(double valor, Conta contaDestino) {
+        this.sacar(valor);
+        contaDestino.depositar(valor);
+    }
 
+    protected void imprimirInfosComuns() {
+        System.out.println(String.format("Agencia: %d", this.agencia));
+        System.out.println(String.format("Numero: %d", this.numero));
+        System.out.println(String.format("Saldo: %.2f", this.saldo));
     }
 }
