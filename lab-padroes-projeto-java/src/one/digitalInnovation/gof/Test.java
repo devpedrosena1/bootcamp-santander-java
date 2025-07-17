1f9e21a3a7129ef577cc0b9b38520a71bf581ce7
devpedrosena1 <devpedrosena1@gmail.com>
2025-07-17 14:14:52 -0300

    entendendo mais sobre o Strategy em design patterns em Java

---
+++ lab-padroes-projeto-java/src/one/digitalInnovation/gof/Test.java
@@ -0,0 +1,43 @@
+package one.digitalInnovation.gof;
+
+import one.digitalInnovation.gof.singleton.SingletonEager;
+import one.digitalInnovation.gof.singleton.SingletonLazy;
+import one.digitalInnovation.gof.singleton.SingletonLazyHolder;
+import one.digitalInnovation.gof.strategy.*;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        // Singleton
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+
+        SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+
+        // Strategy
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+
+        robo.setComportamento(defensivo);
+        robo.mover();
+
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.mover();
+        robo.mover();
+
+    }
+
+}
--- lab-padroes-projeto-java/src/one/digitalInnovation/gof/singleton/SingletonLazyHolder.java
+++ lab-padroes-projeto-java/src/one/digitalInnovation/gof/singleton/SingletonLazyHolder.java
@@ -1,4 +1,4 @@
-package one.digitalInnovation.gof;
+package one.digitalInnovation.gof.singleton;
 
 /**
  * Singleton "Lazy Holder"
